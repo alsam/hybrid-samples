f304a97edee7251222d598ea878ae8d406f4a195
Alexander Samoilov <alexander.samoilov@gmail.com>
2019-12-18 14:50:40 +0300

    add cft2 binding.

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -7,5 +7,6 @@ PYBIND11_MODULE(vossen, m)
     m.def("add", &add);
     m.def("subtract", &subtract);
     m.def("cosfft1", &cosfft1, py::arg("data"), py::arg("n"), py::arg("inverse") = false);
+    m.def("cft2", &cft2, py::arg("data"), py::arg("nn"), py::arg("inverse") = false);
     m.def("scale_by_2", &scale_by_2);
 }
--- tests/math_test.py
+++ tests/math_test.py
@@ -24,6 +24,18 @@ class MainTest(unittest.TestCase):
         # self.assertAlmostEqual(v, vossen.VectorDouble([1.,2,3]))
         self.assertTrue(np.allclose(v, np.array([1.,2,3])))
 
+    def test_cft2(self):
+        m = m_orig = np.array([[1.,2,3],
+                               [4,5,6],
+                               [7,8,9]])
+        m1 = np.array([[ 6.,  4., 10.],
+                       [18., 10., 22.],
+                       [30., 16., 34.]])
+        vossen.cft2(m, 2)
+        self.assertTrue(np.allclose(m, m1))
+        vossen.cft2(m, 2, inverse = True)
+        self.assertTrue(np.allclose(m, m_orig))
+
     def test_scale_by_2(self):
         x = np.array([1.,2.,3.,])
         vossen.scale_by_2(x)
